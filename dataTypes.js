9f6f100870153c34d0a46e60bff04f72a8d40fd3
napat sangthongthat <napat@git.com>
2021-01-13 16:44:55 +0700

    update

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+const people = ["Aaron","Mel","john"];
+const one=1;
+const str="hello world";
+const b =true;
+const employee={
+    firstName: "Anirach",
+    lastname:"Mingkhawn",
+};
+function sayHello(person){
+    consloe.log("Hello"+person.firstName);
+}
+console.log(typeof people);
+console.log(typeof sayHello);
+console.log(employee instanceof array);
+sayHello(employee);
